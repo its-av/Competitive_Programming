d4d2ea5860569123ef0a884ff549fa64e7a35b1c
Avadhesh Gaur <40993196+its-av@users.noreply.github.com>
2022-08-04 14:56:52 +0530

    multiple

---
+++ Template/temp.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+int solve1()
+{
+}
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  int T;
+  cin >> T;
+  while (T--)
+  {
+    cout << solve();
+  }
+  return 0;
+}
