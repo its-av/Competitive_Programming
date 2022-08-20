43b2804039d3a1894ec60dc9b75143e83dad4712
Avadhesh Gaur <40993196+its-av@users.noreply.github.com>
2022-08-20 18:49:48 +0530

    multiple

--- Template/temp.cpp
+++ Template/temp.cpp
@@ -1,9 +1,32 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-int solve()
+void solve()
 {
-  
+  int n,m;
+  cin>>n>>m;
+  vector<vector<int> > vc(n,vector<int> (m));
+  int n_of_1=0;
+  string s;
+  for(int i=0;i<n;i++){
+    cin>>s;
+    for(int j=0;j<m;j++){
+      vc[i][j]=s[j]-'0';
+      n_of_1+=vc[i][j];
+    }
+  }
+  int cnt_1=INT_MAX;
+  int op=0;
+  for(int i=0;i<n-1;i++){
+    for(int j=0;j<m-1;j++){
+      int temp=vc[i][j]+vc[i+1][j]+vc[i][j+1]+vc[i+1][j+1];
+      cnt_1=min(cnt_1,temp);
+      if(cnt_1==0) op=0;
+      else if(cnt_1==1) op=1;
+      else if(cnt_1>=2) op=2;
+    }
+  }
+  cout<<n_of_1-cnt_1+op<<endl;
 }
 int32_t main()
 {
@@ -17,7 +40,7 @@ int32_t main()
   cin >> T;
   while (T--)
   {
-    cout << solve();
+    solve();
   }
   return 0;
 }
